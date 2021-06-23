26b71d34838901ee02364d70c1e284469d6467aa
Mr-Qiu-Lin <59571661+Mr-Qiu-Lin@users.noreply.github.com>
2021-06-23 17:56:26 +0800

    Delete 决赛网页3 directory

--- 决赛网页3/conner.php
+++
@@ -1,5 +0,0 @@
-<?php
-$sql=new mysqli("127.0.0.1","root","","qgt_school");
-if($sql->connect_error){
-    die();
-}
--- 决赛网页3/out.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['name']);
-    setcookie('name','',time()-1);
-    header('location:index.php');
-?>
